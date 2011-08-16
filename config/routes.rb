ea2df2b3776e994b0f9e838e9149127d4a78c1f9
Christopher Sand <christopher.sand@gmail.com>
2011-08-16 00:00:17 -0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 AaaApp::Application.routes.draw do
 
+  get "users/new"
+
   match '/contact',	:to => 'pages#contact'
   match '/about',   	:to => 'pages#about'
   match '/help',    	:to => 'pages#help'
-  match 'home', 	:to => 'pages#home'  
+  match '/home', 	:to => 'pages#home'  
+  match '/signup',	:to => 'users#new'
 
   #match '/contact', :to => 'pages#contact'
   #match '/about', :to => 'pages#about'
